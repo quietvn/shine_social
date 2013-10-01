95f00459fc56c0924a8f1db208c75134e85e83e2
Quan <quietvn@yahoo.com>
2013-10-01 14:58:45 +0700

    V1.0

--- misfit.sql
+++ misfit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 27, 2013 at 10:48 AM
+-- Generation Time: Oct 01, 2013 at 07:53 AM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -34,18 +34,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   `old_score` double unsigned NOT NULL DEFAULT '0',
   `current_score` double unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ids_unique` (`id_shine`,`id_twitter`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `id_shine`, `id_twitter`, `id_group`, `email`, `old_score`, `current_score`) VALUES
-(1, '51c0907251381005bb000003', 'boyd4y', 1, 'winston@misfitwearables.com', 650, 750),
-(2, '523ad606513810d653000fdc', 'loc02', 1, 'loc02@smisfit.com', 827, 827),
-(3, '524545815138103988000001', 'misfitphan', 1, 'quan@misfitwearables.com', 700, 700);
+  `id_server` tinyint(3) unsigned DEFAULT '1',
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
