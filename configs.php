11a41ab4613c895beb272ce0f5a35198acc8f4a9
Ubuntu <ubuntu@ip-10-147-154-126.ec2.internal>
2013-10-07 01:13:19 -0700

    Admin console

--- configs.php
+++ configs.php
@@ -10,11 +10,11 @@ $DB_CONFIGS = array(
 global $DB_CONFIGS;
 
 $TWITTER_CONFIGS = array(
-	'misfitphan' => array(
-		'consumerKey' => 'sSTPGBSB7H7iGVknkDYQ',
-		'consumerSecret' => 'QOfc5TfOaG7VUgKezHudhP09ENRu1CUnl6RGlPA5us',
-		'accessToken' => '1908282224-ooVamTjYHffy8GTsnJTdcwWpT6kWOwmPQl3tS83',
-		'accessTokenSecret' => '2ViGTiwCjel3gIHU5Mk2aL2IiJXTRpAzFGMv2RFYvE'
+	'shinelabs' => array(
+		'consumerKey' => 'xEiIj7K4W95P3DXsldmYIg',
+		'consumerSecret' => 'P1igXYkjB2cqdxsloO2dZbS6j99ciegit2Uhzm7MbQ',
+		'accessToken' => '1908747642-8DduN9aYxoDjr4WkNq6NOueYVrPIP9HM3fNv9Wa',
+		'accessTokenSecret' => 'gGfqJfSYSQg87yooFlvJ9ulLh0EeoO91Q9fiI1o7g'
 	),
 	'shinelabs2' => array(
 			'consumerKey' => 'PkrCTjsBysU3RxA3C4a3bA',
@@ -22,18 +22,37 @@ $TWITTER_CONFIGS = array(
 			'accessToken' => '1921579628-0BvXrPs8c9rPTHDkX1ejT8MLiqAaJi23DBD987I',
 			'accessTokenSecret' => 'tR1Eg1LhcQ4iYw0WP00j0W9pwjUcSi93kCwm9g2ww'
 	),
+	'shinelabs3' => array(
+			'consumerKey' => 'bEey8wMKK8xSXBEMm3Eg',
+			'consumerSecret' => 'tP2hZAvlVqEcYryfewvtiRjCyhUZbv9d7N4WC6d4Q',
+			'accessToken' => '1921587698-TMpASIvlg25hPCuQX6w3jCMGQ5P5RGdFce4C2tj',
+			'accessTokenSecret' => '8Tdui1koKeM1a1XVFysuU8CpexJTlaqj87vq9ZovCI'
+	),
+	'shinelabs4' => array(
+			'consumerKey' => 'OegudIUF6A4KztRzZz6Q',
+			'consumerSecret' => 'ld3F9AthJQEQIlM8ouzAydeqr6ocPwBza3pR43RDhA',
+			'accessToken' => '1924251631-0HtZ6WOeJSCsoeYpjLpASWwcWcp2Ysvdgo8ZVF1',
+			'accessTokenSecret' => 'GcXDN9aUiPLGbxV8zurAZTua4cIwnaVmcqYuqGLNdE'
+	),
+	'shinelabs5' => array(
+			'consumerKey' => 'WznhXVWXUdpmsTpY6XQHw',
+			'consumerSecret' => 'qNodTsDpNe8SY1ceiDtTmW3SS9qYpbmkVFEeob0M',
+			'accessToken' => '1928257328-iCMV7YxMmZMgmxryiJOOhNdYGkKMFIyZXB8kDxe',
+			'accessTokenSecret' => 'gAKpwCoF5lXPogu0Pi7589mbBjMM8F3QmqBXIy0Aico'
+	),
 );
+
 global $TWITTER_CONFIGS;
 
 $MONGO_CONFIG = array(
 	1 => array(
 		'name' => 'Production',
-		'url' => 'mongodb://localhost',
-		'collection' => 'shine'),
+		'url' => 'mongodb://10.144.72.17',
+		'collection' => 'shine_production'),
 	2 => array(
 		'name' => 'Staging',
-		'url' => 'mongodb://localhost',
-		'collection' => 'shine')	
+		'url' => 'mongodb://10.99.65.131',
+		'collection' => 'shine_staging')	
 );
 global $MONGO_CONFIG;
 
@@ -41,3 +60,4 @@ define("TIMEZONE", "US/Pacific");
 define("TIMEZONE_OFFSET", "-07:00");
 
 date_default_timezone_set(TIMEZONE);
+
