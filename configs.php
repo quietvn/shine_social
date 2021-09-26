ebf39bf81258817a0532643a4f602afd672534ce
quietvn <quan@misfitwearables.com>
2021-09-26 14:55:29 +0700

    Update configs.php

--- configs.php
+++ configs.php
@@ -10,10 +10,10 @@ $DB_CONFIGS = array(
 global $DB_CONFIGS;
 
 $TWITTER_CONFIGS = array(
-	'consumerKey' => 'sSTPGBSB7H7iGVknkDYQ',
-	'consumerSecret' => 'QOfc5TfOaG7VUgKezHudhP09ENRu1CUnl6RGlPA5us',
-	'accessToken' => '1908282224-ooVamTjYHffy8GTsnJTdcwWpT6kWOwmPQl3tS83',
-	'accessTokenSecret' => '2ViGTiwCjel3gIHU5Mk2aL2IiJXTRpAzFGMv2RFYvE'
+	'consumerKey' => 'xxx',
+	'consumerSecret' => 'xxx',
+	'accessToken' => 'xxx',
+	'accessTokenSecret' => 'xxx'
 );
 global $TWITTER_CONFIGS;
 
